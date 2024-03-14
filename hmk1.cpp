b71adbe3674a6b384f6bc23e623ee8d90bc94466
Eazed <Eazed_Yu@outlook.com>
2024-03-14 20:49:54 +0800

    添加变量初始化赋值和打印语句

--- hmk1.cpp
+++ hmk1.cpp
@@ -4,20 +4,27 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <fstream>
 using namespace std;
 Date today;
 extern char myName[NAME_LEN];
 extern Book bestBook;
 
 int main() {
+    // 控制台使用UTF-8编码
     system("chcp 65001 > nul");
+    /* <9><10> */
+    // 为上一步中声明的每一个变量进行初始化赋值
+    // 按照下列要求声明若干局部变量 
+    // 为每一种char类型声明一个变量
     char a = 'a'; // 常规字符
     unsigned char b = 'c'; // 无符号字符
     signed char c = 'c'; // 有符号字符
     wchar_t d = L'd'; // 宽字符
     char16_t e = u'e'; // Unicode字符
     char32_t f = U'e'; // Unicode字符
-
+    
+    // 为每一种整数类型声明一个变量
     short g = 1; // 短整型
     unsigned short h = 2; // 无符号短整型
     int i = 3; // 整型
@@ -26,12 +33,13 @@ int main() {
     unsigned long l = 6UL; // 无符号长整型
     long long m = 7LL; // 长长整型
     unsigned long long n = 8ULL; // 无符号长长整型
-
+    
+    // 为每一种浮点类型声明一个变量
     float o = 1.0f; // 单精度浮点数
     double p = 2.0; // 双精度浮点数
     long double q = 3.0L; // 扩展精度浮点数
     
-
+    /* <18> */
     int _weekday;
     int _month;
     int _year;
@@ -65,7 +73,6 @@ int main() {
     bestBook.price = _bookPrice;
     bestBook.u.library_id = _bookId;
 
-    // Print all the input information
     cout << "年份: " << _year << endl;
     cout << "月份: " << _month << endl;
     cout << "星期: " << _weekday << endl;
@@ -75,4 +82,91 @@ int main() {
     cout << "最好的书的价格: " << _bookPrice << endl;
     cout << "最好的书的id: " << _bookId << endl;
     system("pause");
+    
+    /* <19> */
+    int x;
+    const int* xp;
+    const int* xp2 = new int[5];
+    int* ar1[5];
+    
+    size_t (*fp1)(const char*) = strlen;
+    auto fp2 = strncpy;
+    
+    /* <26> */
+    cout << a << b << c << d << e << f << f << endl;
+    
+    /* <27> */
+    cout << INT_MAX << endl << LONG_LONG_MIN << endl;
+    
+    /* <28> */
+    int number {42};
+    
+    /* <29> */
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+    cout << g << endl;
+    cout << h << endl;
+    cout << i << endl;
+    cout << j << endl;
+    cout << k << endl;
+    cout << l << endl;
+    cout << m << endl;
+    cout << o << endl;
+    cout << p << endl;
+    cout << q << endl;
+    
+    /* <30> */
+    // 打印十进制
+    cout << "十进制: " << number << endl;
+    // 打印八进制
+    cout << "八进制: " << oct << number << endl;
+    // 打印十六进制
+    cout << "十六进制: " << hex << number << endl;
+    
+    /* <31> */
+    char ch = 'A';
+    // 打印字符
+    std::cout << "字符: " << ch << std::endl;
+    // 打印对应的整型值
+    std::cout << "整型值: " << static_cast<int>(ch) << std::endl;
+    
+    /* <32> */
+    cout << sizeof (ar1) << endl;
+    
+    /* <33> */
+    char str1[100];
+    cin >> str1;
+    
+    /* <34> */
+    string str2;
+    cin >> str2;
+    
+    char ch2;
+    ch2 = getchar();
+    
+    
+    // 打开文件info.txt，并写入myName变量的内容
+    ofstream outFile("info.txt");
+    if (outFile.is_open()) {
+        outFile << myName << endl;
+        outFile.close(); // 关闭文件
+    } else {
+        cout << "无法打开文件info.txt进行写入" << endl;
+    }
+
+    // 打开文件info.txt，并读取内容，然后打印到屏幕上
+    ifstream inFile("info.txt");
+    string line;
+    if (inFile.is_open()) {
+        while (getline(inFile, line)) {
+            cout << line << endl;
+        }
+        inFile.close(); // 关闭文件
+    } else {
+        cout << "无法打开文件info.txt进行读取" << endl;
+    }
 }
--- tools.h
+++ tools.h
@@ -1,7 +1,11 @@
 // tools.h是与tools.cpp对应的头⽂件
+/* <8> */
+// 声明变量
 #define TITLE_LEN 80
 const int NAME_LEN = 50;
 
+/* <11> */
+// 声明一个结构体Book
 typedef struct {
     char title[TITLE_LEN];      // 书籍标题
     char authors[NAME_LEN];     // 作者名字
@@ -13,10 +17,16 @@ typedef struct {
     
 } Book;
 
+/* <11> */
+// 声明一个枚举类型  Weekday
 typedef enum {Mon, Tue, Wed, Thu, Fri, Sat, Sun} Weekday;
 
+/* <12> */
+// 声明一个枚举类型  Month
 typedef enum {Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec} Month;
 
+/* <13> */
+// 声明一个结构体  Date
 typedef struct {
     unsigned int year;
     Month month;
