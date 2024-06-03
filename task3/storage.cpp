ab5bffecaab7de8915f75995bb0fee0561c6a716
Eazed <Eazed_Yu@outlook.com>
2024-06-03 16:35:52 +0800

    作业3初始化

---
+++ task3/storage.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eazed on 24-6-3.
+//
+
+#include "storage.h"
---
+++ task3/storage.h
@@ -0,0 +1,13 @@
+//
+// Created by Eazed on 24-6-3.
+//
+
+#ifndef TASK3_STORAGE_H
+#define TASK3_STORAGE_H
+
+
+class storage {
+};
+
+
+#endif//TASK3_STORAGE_H
