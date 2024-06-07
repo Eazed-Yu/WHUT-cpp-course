e3bc8859635e79c292e354c367ce3e51381a5966
Eazed <Eazed_Yu@outlook.com>
2024-06-07 15:23:07 +0800

    作业3 完整singlelist

--- task3/storage.h
+++ task3/storage.h
@@ -9,47 +9,27 @@
 #include <iostream>
 
 
-template <typename T>
+template<typename T>
 class storage {
 public:
     virtual ~storage() = default;
-    storage(unsigned int size = 0) {
-        this->size = size;
-    }
 
-    unsigned int getSize() { return size; }
-    bool isEmpt() const {return size == 0; }
-    bool isFull() const { return size == MAX_SIZE; }
+    virtual unsigned int getSize() = 0;
+
+    virtual bool isEmpty() = 0;
+
+    virtual bool isFull() = 0;
 
     virtual bool add(T n);
+
     virtual bool remove(T &);
-    virtual bool find(T item) = 0; // 查找元素
-    virtual void print() = 0; // 打印元素
-protected:
-    unsigned int size;   // 容器大小
-    static const unsigned int MAX_SIZE = 1000;
+
+    virtual bool find(T item) = 0;
+
+    virtual void print() = 0;
 };
 
-template <typename T>
-class Stack : public storage<T> {
-public:
-    virtual ~Stack() = default;
-    virtual bool push(T item) = 0; // 压栈
-    virtual bool pop(T &n) = 0; // 出栈
-    bool add(T n) { push(); }
-    bool remove(T &n) { return pop(n); }
-};
-
-template <typename T>
-class ArrStack : public Stack<T> {
-public:
-    void push(T item) override;
-    T pop() override;
-private:
-
-};
-
-template <typename T>
+template<typename T>
 class List : public storage<T> {
     virtual ~List() = default;
 
@@ -62,32 +42,74 @@ class List : public storage<T> {
     virtual bool delete_tail(T &n) = 0;
 };
 
-template <typename T>
+template<typename T>
 class SingleList : public List<T> {
 public:
     SingleList();
+
     ~SingleList();
 
-    bool append(T n) override;
-    bool prepend(T n) override;
-    bool delete_head(T &n) override;
-    bool delete_tail(T &n) override;
-    bool add(T) override;
-    bool remove(T &) override;
+    unsigned int getSize() override;
+
+    bool isEmpty() override;
+
+    bool isFull() override;
+
+    bool add(T n) override { return append(n); };
+
+    bool remove(T &n) override { return delete_head(n); };
+
     void print() override;
+
     bool find(T n) override;
+
+    bool append(T n) override;
+
+    bool prepend(T n) override;
+
+    bool delete_head(T &n) override;
+
+    bool delete_tail(T &n) override;
+
 private:
-    struct Node
-    {
-        Node(const T& data)  : data(data), next(nullptr) {
+    struct Node {
+        Node(const T &data) : data(data), next(nullptr) {
 
         }
+
         T data;
         Node *next;
     };
 
     Node *head;
     Node *tail;
+    unsigned int size;
+    unsigned int MAX_SIZE = 1000;
 };
+
+
+template<typename T>
+class Stack : public storage<T> {
+public:
+    virtual ~Stack() = default;
+
+    virtual bool push(T item) = 0; // 压栈
+    virtual bool pop(T &n) = 0; // 出栈
+    bool add(T n) { push(n); }
+
+    bool remove(T &n) { return pop(n); }
+};
+
+template<typename T>
+class ArrStack : public Stack<T> {
+public:
+    void push(T item) override;
+
+    T pop() override;
+
+private:
+
+};
+
 
 #endif//TASK3_STORAGE_H
--- task3/storageImpl.h
+++ task3/storageImpl.h
@@ -8,13 +8,131 @@
 #include "storage.h"
 
 template<typename T>
-void ArrStack<T>::push(T item) {
+SingleList<T>::SingleList() : head(nullptr), tail(nullptr), size(0) {}
 
+template<typename T>
+SingleList<T>::~SingleList() {
+    Node *current = head;
+    while (current) {
+        Node *tmp = current;
+        current = current->next;
+        delete tmp;
+    }
+    head = nullptr;
+    tail = nullptr;
 }
 
 template<typename T>
-T ArrStack<T>::pop() {
-    return nullptr;
+unsigned int SingleList<T>::getSize() {
+    return size;
+}
+
+template<typename T>
+bool SingleList<T>::isEmpty() {
+    return size == 0;
+}
+
+template<typename T>
+bool SingleList<T>::isFull() {
+    return size == MAX_SIZE;
+}
+
+template<typename T>
+bool SingleList<T>::append(T n) {
+    if (!isFull()) {
+        Node *node = new Node(n);
+        if (tail) {
+            tail->next = node;
+            tail = node;
+        } else {
+            head = tail = node;
+        }
+        size++;
+        return true;
+    }
+}
+
+template<typename T>
+bool SingleList<T>::prepend(T n) {
+    if (!isFull()) {
+        Node *node = new Node(n);
+        if (head) {
+            node->next = head;
+            head = node;
+        } else {
+            head = tail = node;
+        }
+        size++;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+bool SingleList<T>::delete_head(T &n) {
+    if (head) {
+        Node *temp = head;
+        head = head->next;
+
+        n = temp->data;
+        delete temp;
+
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        size--;
+        return true;
+    }
+    return false;
+}
+
+
+template<typename T>
+bool SingleList<T>::delete_tail(T &n) {
+    if (tail) {
+        n = tail->data;
+        if (tail == head) {
+            delete tail;
+            head = tail = nullptr;
+        } else {
+            Node *current = head;
+            while (current->next != tail) {
+                current = current->next;
+            }
+            delete tail;
+            tail = current;
+            tail->next = nullptr;
+        }
+        size--;
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+void SingleList<T>::print() {
+    Node *current = head;
+    std::cout << "[";
+    while (current != nullptr) {
+        std::cout << current->data;
+        if (current->next != nullptr) {
+            std::cout << ", ";
+        }
+        current = current->next;
+    }
+    std::cout << "]" << std::endl;
+}
+
+template<typename T>
+bool SingleList<T>::find(T n) {
+    Node *current = head;
+    while (current) {
+        if (current->data == n) {
+            return true;
+        }
+        current = current->next;
+    }
+    return false;
 }
 
 #endif //TASK3_STORAGEIMPL_H
